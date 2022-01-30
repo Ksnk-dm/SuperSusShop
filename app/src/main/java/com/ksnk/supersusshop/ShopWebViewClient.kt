fcd997538b1b6367003fd1ea5043fffb7ed97bb3
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2022-01-30 17:47:27 +0200

    ref

--- app/src/main/java/com/ksnk/supersusshop/ShopWebViewClient.kt
+++ app/src/main/java/com/ksnk/supersusshop/ShopWebViewClient.kt
@@ -5,8 +5,8 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class ShopWebViewClient: WebViewClient() {
-    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-        view?.loadUrl(view.url.toString())
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
         return true
     }
 
